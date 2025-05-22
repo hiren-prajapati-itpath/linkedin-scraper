6d84e882594e5c7a096d427613e2acc26b4f7b61
Hiren Prajapati <hirenpr.itpath@gmail.com>
2025-05-22 17:06:35 +0530

    remove commented-out route examples from index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,5 @@ const router = express.Router();
 // Register all routes
 router.use('/profile', profileRoutes);
 
-// Add more route categories here
-// Example:
-// router.use('/auth', authRoutes);
-// router.use('/companies', companyRoutes);
 
 module.exports = router;
